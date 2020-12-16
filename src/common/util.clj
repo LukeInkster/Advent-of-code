4914f3c1fe0b1f3efd904a6dc30b16e8bf353ffb
Luke Inkster <luke.inkster@gmail.com>
2020-12-16 10:42:59 -0500

    Add day 16

---
+++ src/common/util.clj
@@ -0,0 +1,18 @@
+(ns common.util)
+
+
+(defn mapm
+  "Like map, maps map-fn over the values of input and returns a map from original keys to new values."
+  [map-fn coll]
+  (into {} (map (fn [[k v]] [k (map-fn v)])) coll))
+
+
+(defn mapping
+  "Constructs a map from (key-fn x) => (val-fn x) for each x in coll.
+  Duplicate keys will overwrite previous entries."
+  [key-fn val-fn coll]
+  (persistent!
+    (reduce (fn [m x]
+              (assoc! m (key-fn x) (val-fn x)))
+            (transient {})
+            coll)))
