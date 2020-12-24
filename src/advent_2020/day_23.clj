85a536c7694b498ce07bd3dd8f73f9dc0d01183c
Luke Inkster <luke.inkster@gmail.com>
2020-12-23 23:48:11 -0500

    Add a faster version of day 23

--- src/advent_2020/day_23.clj
+++ src/advent_2020/day_23.clj
@@ -49,3 +49,46 @@
 
 (def large-input
   "643719258")
+
+
+(defn play-fast
+  "Just like `play`, but much uglier and 10+ times faster. ~7 seconds for part 2.
+  `cups` must be a vector"
+  [cups ^long rounds]
+  (let [cup->next (long-array (inc (count cups)))
+        _ (doseq [[^long k ^long v] (into [[(peek cups) (first cups)]] (partition 2 1 cups))]
+            (aset cup->next k v))
+        highest (long (count cups))
+        possible-targets (object-array (inc (count cups)))
+        _ (doseq [cup cups]
+            (aset possible-targets cup (long-array (map (fn [^long x]
+                                                          (if (< x 1)
+                                                            (mod (unchecked-dec x) (unchecked-inc highest)) x))
+                                                        (range (unchecked-dec cup) (- cup 5) -1)))))]
+    (loop [^long current (first cups)
+           i 0]
+      (if (= i rounds)
+        cup->next
+        (let [a (aget cup->next current)
+              b (aget cup->next a)
+              c (aget cup->next b)
+              target (long (first (remove (fn [^long x] (some (fn [^long y] (= x y)) [a b c]))
+                                          ^longs (aget possible-targets current))))]
+          (aset cup->next current (aget cup->next c))
+          (aset cup->next c (aget cup->next target))
+          (aset cup->next target a)
+          (recur (aget cup->next current) (unchecked-inc i)))))))
+
+
+(defn part-1-fast
+  [input]
+  (let [^longs cup->next (play-fast (read-cups input) 100)]
+    (string/join "" (take (dec (dec (count cup->next))) (iterate #(aget cup->next %) (aget cup->next 1))))))
+
+
+(defn part-2-fast
+  [input]
+  (let [base-cups (read-cups input)
+        cups (into base-cups (range (inc (count base-cups)) 1000001))
+        ^longs cup->next (play-fast cups 10000000)]
+    (* (aget cup->next 1) (aget cup->next (aget cup->next 1)))))
